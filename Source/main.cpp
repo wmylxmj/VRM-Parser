28170a82c2577405a70eb22dba63be9ef3bbc50d
wmylxmj <1397322329@qq.com>
2025-02-28 22:39:15 +0800

    初始化项目

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "Model/VrmModel.h"
+
 int main() {
+    VrmModel model(R"(E:\vrm\20220331_1455\20220331_1455\base body\black cat base body v3.5.0.vrm)");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
