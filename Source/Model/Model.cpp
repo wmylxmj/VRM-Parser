044192c4f05efe06283234913d2646bd7ce86a9d
wmylxmj <1397322329@qq.com>
2025-02-28 22:13:50 +0800

    初始化项目

---
+++ Source/Model/Model.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 13973 on 25-2-28.
+//
+
+#include "Model.h"
+
+Model::Model(const std::string& filePath) {
+}
---
+++ Source/Model/Model.h
@@ -0,0 +1,13 @@
+//
+// Created by 13973 on 25-2-28.
+//
+
+#pragma once
+
+#include <string>
+
+class Model {
+
+public:
+    explicit Model(const std::string& filePath);
+};
