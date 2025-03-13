42809d83e3227ce5275dbf1f97b354176aa53d47
wmylxmj <1397322329@qq.com>
2025-03-13 23:24:17 +0800

    Assimp读取测试

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,7 +3,16 @@
 #include "Model/VrmModel.h"
 
 int main() {
+    std::cout << "Loading Model..." << std::endl;
     VrmModel model(R"(E:\vrm\20220331_1455\20220331_1455\base body\black cat base body v3.5.0.vrm)");
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Model Loaded." << std::endl;
+    std::cout << "Num Vertices: " << model.vertices.size() << std::endl;
+    std::cout << "Num Triangle Faces: " << model.indices.size() / 3 << std::endl;
+    std::cout << "Num Meshes: " << model.meshEntries.size() << std::endl;
+    std::cout << "Num Bones: " << model.bones.size() << std::endl;
+    std::cout << "Bone Bone Hierarchy: " << std::endl;
+    for (auto &[fst, snd] : model.boneIndexMapping) {
+        std::cout << fst << ": " << snd << "->" << model.bones[snd].parentIndex <<  std::endl;
+    }
     return 0;
 }
