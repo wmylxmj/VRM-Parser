851b2631f5f9361d7bb1cb7208b44ce0b72e17c8
wmylxmj <1397322329@qq.com>
2025-03-24 21:46:39 +0800

    新增交互几何

---
+++ Source/InteractiveGeometry/JointBall.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 13973 on 25-3-24.
+//
+
+#include "JointBall.h"
---
+++ Source/InteractiveGeometry/JointBall.h
@@ -0,0 +1,19 @@
+//
+// Created by 13973 on 25-3-24.
+//
+
+#pragma once
+
+#include "../Core/PrecompiledHeader.h"
+
+class JointBall {
+
+public:
+
+    struct Vertex {
+        
+    };
+};
+
+
+
